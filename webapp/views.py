2d56203d3c686e9023365eb5dbb444fe5af706b3
matthew <thisiswbb@gmail.com>
2023-09-08 19:48:25 +0100

    wiki complete probably

--- webapp/views.py
+++ webapp/views.py
@@ -622,3 +622,7 @@ def logged_in(selection):
                 return redirect('/logout')
 
     return render_template('new-login-screen.html', username = username, scores = scores, rank = rank, globalrank = globalrank, classrank = classrank, points = points, CSI_attempts = CSI_attempts, best_csi = best_csi, best_csi_time = bestCSITIME)
+
+@views.route('/wiki')
+def wiki():
+    return render_template('wiki.html')
